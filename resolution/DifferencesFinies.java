e472140b54091911bcdf7af990d84f64cf20c722
binel01 <binelimanga@gmail.com>
2016-04-20 12:33:02 +0100

    Gestion de la bibliothèque commons maths ajoutée

--- resolution/DifferencesFinies.java
+++ resolution/DifferencesFinies.java
@@ -7,12 +7,20 @@ package resolution;
 
 import Jama.Matrix;
 import fonction.IFonction;
-import java.util.Arrays;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 import org.ojalgo.access.Access2D.Builder;
 import org.ojalgo.matrix.BasicMatrix;
 import org.ojalgo.matrix.BasicMatrix.Factory;
 import org.ojalgo.matrix.PrimitiveMatrix;
-import org.ojalgo.random.Weibull;
+
+
+
 
 /**
  *
@@ -23,7 +31,6 @@ public class DifferencesFinies extends AResolution{
     @Override
     public double[] solve(IFonction fonction, double u0, double u1, double[] mesh){
         return solveOjAlgo(fonction, u0, u1, mesh);
-        //return solveJama(fonction, u0, u1, mesh);
     }
 
     public double[] solveJama(IFonction fonction, double u0, double u1, double[] mesh) {
@@ -119,4 +126,60 @@ public class DifferencesFinies extends AResolution{
         //Résolution de l'équation
         return aMatrix.solve(bMatrix).toRawCopy1D();
     }
+    
+    public double[] solveCommonMaths(IFonction fonction, double u0, double u1, double[] mesh) {
+        int n = mesh.length;
+        if(n==0){
+            return new double[0];
+        }
+        
+        double b[];
+        try{
+            b = fonction.evaluer(mesh);
+        }catch(Exception e){
+            return null;
+        }
+        
+        double h = mesh[0];
+        for(int i=0; i<n-1; i++){
+            h = Math.max(h, mesh[i+1] - mesh[i]);
+        }
+        h = Math.max(h, 1 - mesh[n-1]);
+        
+        double hCarre = h * h;
+        double a[][] = new double[n][n];
+        double temp1 = -1.0/hCarre;
+        double temp2 = 2.0/hCarre;
+        
+        a[0][0] = temp2;
+        if(n>1){
+            for(int i=1; i<n; i++){
+                a[i][i] = temp2;
+                a[i][i-1] = temp1;
+                a[i-1][i] = temp1;
+            }
+        }
+        //Matrix aMatrix = new Matrix(a);
+        
+        // Création de la matrice A
+        RealMatrix matrice = new Array2DRowRealMatrix(a, false);
+        DecompositionSolver solver = new LUDecomposition(matrice).getSolver();
+        
+        
+        //Construction du vecteur b
+        b[0] = b[0] + u0 / hCarre;
+        b[n-1] = b[n-1] + u1 / hCarre;        
+        
+        RealVector bVect = new ArrayRealVector(b, false);
+        //Matrix bMatrix = new Matrix(b, n);
+        
+        //Résolution de l'équation
+        
+        //RealVector res = solver.solve(bVect);
+        // double[] res1 = res.toArray();
+        
+        return solver.solve(bVect).toArray();
+    }   
+
+    
 }
--- resolution/VolumesFinis.java
+++ resolution/VolumesFinis.java
@@ -6,6 +6,12 @@
 package resolution;
 
 import fonction.IFonction;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 import org.ojalgo.access.Access2D.Builder;
 import org.ojalgo.matrix.BasicMatrix;
 import org.ojalgo.matrix.PrimitiveMatrix;
@@ -23,7 +29,7 @@ public class VolumesFinis extends AResolution {
         return solveOjalgo(fonction, u0, u1, mesh);
     }
     
-    private double[] solveOjalgo(IFonction fonction, double u0, double u1, double[] mesh) {    
+    public double[] solveOjalgo(IFonction fonction, double u0, double u1, double[] mesh) {    
         double[] result = null;
         int n = mesh.length;
         
@@ -36,41 +42,24 @@ public class VolumesFinis extends AResolution {
         double[] hi2 = new double[n+1];
         double[] hi = new double[n];
         
-        /*
-        mesh2[0] = 0;
-        mesh2[n+1] = 1;
-        
-        for (int i=0; i<n; i++)  
-            mesh2[i+1] = mesh[i];
-        
-        hi2[0] = mesh2[1];
-        for (int i=1; i<n; i++) {
-            hi2[i] = mesh2[i+1] - mesh2[i]; // hi2[i] = mesh[i] - mesh[i-1];
-            hi[i-1] = 0.5*(hi2[i] + hi2[i-1]);
-        }
-        hi2[n] = mesh2[n+1] - mesh2[n];
-        hi[n-1] = 0.5*(hi2[n] - hi2[n-1]);
-        //*/
-        
         hi2[0] = mesh[0];
         for (int i=1; i<n; i++) {
             hi2[i] = mesh[i] - mesh[i-1];
             hi[i-1] = 0.5*(hi2[i] + hi2[i-1]);
         }
         hi2[n] = 1 - mesh[n-1];
-        hi[n-1] = 0.5*(hi2[n] - hi2[n-1]);
+        hi[n-1] = 0.5*(hi2[n] + hi2[n-1]);
         
         // Construction de la matrice A     
         double temp = 1.0/hi2[0] + 1.0/hi2[1];
-        double temp1 = 0.0;
-        double temp2 = 0.0;
         
-        System.out.println(n);
         
         final BasicMatrix.Factory<PrimitiveMatrix> tmpFactory = PrimitiveMatrix.FACTORY;
         Builder<PrimitiveMatrix> tmpBuilder = tmpFactory.getBuilder(n, n);
         tmpBuilder.set(0, 0, temp);
+        
         if(n>1){
+            //tmpBuilder.set(0, 1, -1.0/hi2[0]);
             for (int i = 1; i < n; i++) {
                 tmpBuilder.set(i, i, (1.0/hi2[i] + 1.0/hi2[i+1]));
                 tmpBuilder.set(i, i-1, -1.0/hi2[i]);
@@ -105,5 +94,80 @@ public class VolumesFinis extends AResolution {
         
         //Résolution de l'équation
         return aMatrix.solve(bMatrix).toRawCopy1D();
+    }
+    
+    public double[] solveCommonMaths(IFonction fonction, double u0, double u1, double[] mesh) {    
+        double[] result = null;
+        int n = mesh.length;
+        
+        if (n == 0) {
+            return result;
+        }
+        
+
+        // Calcul des h(i) et h(i+1/2)
+        double[] hi2 = new double[n+1];
+        double[] hi = new double[n];
+        
+        hi2[0] = mesh[0];
+        for (int i=1; i<n; i++) {
+            hi2[i] = mesh[i] - mesh[i-1];
+            hi[i-1] = 0.5*(hi2[i] + hi2[i-1]);
+        }
+        hi2[n] = 1 - mesh[n-1];
+        hi[n-1] = 0.5*(hi2[n] + hi2[n-1]);
+        
+        // Construction de la matrice A     
+        double temp = 1.0/hi2[0] + 1.0/hi2[1];
+    
+        
+        
+        // Création de la matrice A
+        RealMatrix matrice = new Array2DRowRealMatrix(n, n);
+        matrice.addToEntry(0, 0, temp);
+        
+        if(n>1){
+            //matrice.addToEntry(0, 1, -1.0/hi2[0]);
+            for (int i = 1; i < n; i++) {
+                matrice.addToEntry(i, i, (1.0/hi2[i] + 1.0/hi2[i+1]));
+                matrice.addToEntry(i, i-1, -1.0/hi2[i]);
+                matrice.addToEntry(i-1, i, -1.0/hi2[i+1]);
+            }
+        }
+        
+        DecompositionSolver solver = new LUDecomposition(matrice).getSolver();
+        
+        //Construction du vecteur b
+        double b[];
+        try{
+            b = fonction.evaluer(mesh);
+            
+            for (int i=0; i<n; i++) {
+                b[i] = b[i] * hi[i];
+            }
+        }catch(Exception e){
+            return null;
+        }
+        
+                
+        b[0] = b[0] + u0 / hi2[0];
+        b[n-1] = b[n-1] + u1 / hi2[n-1];
+        
+        
+        
+        /*
+        RealVector bVect = new ArrayRealVector(n);
+        for (int i=0; i<n; i++)
+            bVect.setEntry(i, b[i]);
+        //*/
+        ArrayRealVector bVect = new ArrayRealVector(b);
+        //Matrix bMatrix = new Matrix(b, n);
+        
+        //Résolution de l'équation
+        
+        //RealVector res = solver.solve(bVect);
+        // double[] res1 = res.toArray();
+        
+        return solver.solve(bVect).toArray();
     }
 }
